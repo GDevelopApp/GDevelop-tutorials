3cc363a5d9fb67cf892e04fb8c3055f8e06eb171
Aurélien Vivet <bouh.vivez@gmail.com>
2024-06-19 18:10:11 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -83,6 +83,7 @@ axios
         'https://resources.gdevelop-app.com/in-app-tutorials/templates/object3d/game.json',
         'https://resources.gdevelop-app.com/in-app-tutorials/knightPlatformer.json',
         'https://resources.gdevelop-app.com/in-app-tutorials/templates/knightPlatformer/game.json',
+        'https://resources.gdevelop-app.com/in-app-tutorials/templates/fireABullet.json',
         'https://resources.gdevelop-app.com/in-app-tutorials/templates/fireABullet/game.json',
       ],
     },
